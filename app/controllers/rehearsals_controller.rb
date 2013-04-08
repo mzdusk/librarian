e8c02620edad4fbb87e936240d294ae66eab7b11
Yusuke Matsuda <y.m.pinefield+github@gmail.com>
2013-04-08 23:06:07 +0900

    Use pagination

--- app/controllers/rehearsals_controller.rb
+++ app/controllers/rehearsals_controller.rb
@@ -1,5 +1,5 @@
 class RehearsalsController < ApplicationController
   def index
-    @rehearsals = Rehearsal.where('date >= ?', Time.now).order('date')
+    @rehearsals = Rehearsal.where('date >= ?', Time.now).order('date').page(params[:page]).per(10)
   end
 end
--- spec/controllers/rehearsals_controller_spec.rb
+++ spec/controllers/rehearsals_controller_spec.rb
@@ -2,16 +2,49 @@ require 'spec_helper'
 
 describe RehearsalsController do
   describe "GET index" do
-    it "assigns @rehearsals" do
+    before do
       get :index
+    end
+
+    it "assigns @rehearsals" do
       expect(assigns(:rehearsals)).not_to be_nil
     end
 
-    it "@rehearsals has no past rehearsals" do
-      create_list :future_rehearsal, 3
-      create_list :past_rehearsal, 2
-      get :index
-      expect(assigns(:rehearsals)).to be_all { |rehearsal| rehearsal.date > Time.now }
+    context "when past rehearsals exist" do
+      before do
+        create_list :future_rehearsal, 3
+        create_list :past_rehearsal, 2
+      end
+
+      it "@rehearsals has no past rehearsals" do
+        expect(assigns(:rehearsals)).to be_all { |rehearsal| rehearsal.date > Time.now }
+      end
+    end
+
+    context "when more than 10 future rehearsals exists" do
+      before do
+        create_list :future_rehearsal, 15
+      end
+
+      context "page 1" do
+        before do
+          get :index, :page => 1
+        end
+
+        it "@rehearsal has 10 rehearsals" do
+          expect(assigns(:rehearsals)).to have(10).items
+        end
+      end
+
+      context "page 2" do
+        before do
+          get :index, :page => 2
+        end
+
+        it "@rehearsal has 5 rehearsals" do
+          expect(assigns(:rehearsals)).to have(5).items
+        end
+      end
     end
   end
 end
